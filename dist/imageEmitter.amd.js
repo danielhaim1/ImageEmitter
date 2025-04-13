6439a7e23875d63be2575fe1d2b25bc5e8587c5b
Daniel Haim <2808046+danielhaim1@users.noreply.github.com>
2025-04-13 16:25:03 +0200

    Release: v$(node -p "require(./package.json).version")

--- dist/imageEmitter.amd.js
+++ dist/imageEmitter.amd.js
@@ -1,5 +1,5 @@
 /*!
- * @danielhaim/imageemitter - v1.2.4 - 2025-04-13
+ * @danielhaim/imageemitter - v1.2.5 - 2025-04-13
  * git+https://github.com/danielhaim1/imageemitter.git
  * Copyright (c) 2025 Daniel Haim, Licensed MIT
  */(()=>{"use strict";define(["./src/index.js","./src/ImageHelper.js"],(function(e,t){function o(e){return o="function"==typeof Symbol&&"symbol"==typeof Symbol.iterator?function(e){return typeof e}:function(e){return e&&"function"==typeof Symbol&&e.constructor===Symbol&&e!==Symbol.prototype?"symbol":typeof e},o(e)}"object"===("undefined"==typeof module?"undefined":o(module))&&module.exports&&(module.exports={default:e.ImageEmitter,ImageEmitter:e.ImageEmitter,ImageHelper:t.ImageHelper}),"object"===("undefined"==typeof window?"undefined":o(window))&&(window.ImageEmitter=e.ImageEmitter,window.ImageHelper=t.ImageHelper)}))})();
